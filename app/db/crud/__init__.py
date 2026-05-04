f8c2c2820bfa4f58d136e6c1954959e8e9df09fe
Treizd <sqeshexaero@gmail.com>
2026-05-04 20:49:15 +0300

    feat: added __init__.py example for crud methods

---
+++ app/db/crud/__init__.py
@@ -0,0 +1,6 @@
+from .challenges import zxc # все методы через запятую импортни
+from .messages import zxc # все методы через запятую импортни
+from .sessions import zxc # все методы через запятую импортни
+from .users import zxc # все методы через запятую импортни
+
+__all__ = [] # сюда строками впиши методы все
